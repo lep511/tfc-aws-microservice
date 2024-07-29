2dc0443e39f8f29c50faeef79a3b36e9fcd01f59
lep511 <estebanpbuday@gmail.com>
2024-07-28 21:29:53 -0300

    update

--- microservice-1.tf
+++ microservice-1.tf
@@ -1,7 +1,4 @@
 # DynamoDB Table
-locals {
-  example_data = csvdecode(file("data/example_orders.csv"))
-}
 resource "random_pet" "table_name" {
   prefix    = "orders"
   separator = "_"
@@ -18,43 +15,16 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
 
   attribute {
     name = "SourceOrderID"
-    type = "N"
+    type = "S"
   }
 
   attribute {
     name = "SourceItemID"
-    type = "N"
+    type = "S"
   }
   tags = {
     "Name"        = "dynamodb-table-1"
     "Environment" = "production"
     "Project"     = "${var.default_tags.project}"
-  }
-}
-
-resource "aws_dynamodb_table_item" "example" {
-  for_each = var.load_example_data ? { for row in local.example_data : row.SourceOrderID => row } : {}
-
-  table_name = random_pet.table_name.id
-  hash_key   = "SourceOrderID"
-  range_key  = "SourceItemID"
-
-  item = <<EOF
-{
-    "SourceOrderID": {"N": "${each.value.SourceOrderID}"},
-    "SourceItemID": {"N": "${each.value.SourceItemID}"},
-    "DestinationName": {"S": "${each.value.DestinationName}"},
-    "ItemSKU": {"S": "${each.value.ItemSKU}"},
-    "ShipToName": {"S": "${each.value.ShipToName}"},
-    "ShipToCompany": {"S": "${each.value.ShipToCompany}"},
-    "ShipToAddress": {"S": "${each.value.ShipToAddress}"},
-    "ShipToTown": {"S": "${each.value.ShipToTown}"},
-    "CarrierCode": {"S": "${each.value.CarrierCode}"},
-    "CarrierService": {"S": "${each.value.CarrierService}"},
-}
-EOF
-
-  lifecycle {
-    ignore_changes = [item]
   }
 }
